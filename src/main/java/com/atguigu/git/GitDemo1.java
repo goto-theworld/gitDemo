8dced7fccf278ae73a45caf87c5cd31136bf0aea
Dior <Dior@atguigu.com>
2022-10-10 19:06:54 +0800

    commit1

--- src/main/java/com/atguigu/git/GitDemo1.java
+++ src/main/java/com/atguigu/git/GitDemo1.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class GitDemo1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        System.out.println("hello git");
     }
 }
